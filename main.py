691c0e68dbc31b132b7c5597227a6ebfa3292abc
NoarMarcus <noar.marcus@gmail.com>
2023-07-01 13:38:09 +0300

    added model and todo data loader

--- main.py
+++ main.py
@@ -25,20 +25,39 @@ y_test = test_data["genre"]
 labels = y_train.unique()
 y_train = y_train.replace(labels, np.arange(len(labels)))
 criterion = torch.nn.CrossEntropyLoss()# defining criterion
-if(1):# testing criterion
+if(0):# testing criterion
     random_labels = torch.ones((len(x_train), len(labels)))
     loss = criterion(random_labels, torch.tensor(y_train))
     print(loss)
 
 ##pre process
-
-
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased') # define pre-trained tokenizer
 sample = x_train[0]
 x_train = x_train.apply(preprocessW,args = (400,tokenizer))# convert to lower case and add symbols to data 
 x_test = x_test.apply(preprocessW,args = (400,tokenizer))
 
-if(1):# testing tokenizer
+if(0):# testing tokenizer
     print("""original sentence:\n{}\n preprocessed sentence:\n{}\n 
           tokenized sentence:\n{}\n""".format(sample,x_train[0],tokenizer.convert_tokens_to_ids(x_train[0])))
+
+# TODO: add data loader
+## TODO: create CustomDataset
+## TODO: add data loader
+
+
+## Creating the class moodel to fine tune and passing it to device
+class MovieClassifier(torch.nn.Module): 
+    def __init__(self):
+        super(MovieClassifier, self).__init__()
+        self.l1 = transformers.BertModel.from_pretrained('bert-base-uncased')
+        self.l2 = torch.nn.Dropout(0.3)
+        self.l3 = torch.nn.Linear(768, 10)
     
+    def forward(self, ids, mask, token_type_ids):
+        _, output_1= self.l1(ids, attention_mask = mask, token_type_ids = token_type_ids)
+        output_2 = self.l2(output_1)
+        output = self.l3(output_2)
+        return output
+
+model = MovieClassifier()
+model.to(device)
